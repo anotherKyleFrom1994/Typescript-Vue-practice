8f84247bc71ed2658fde7169539a63735776c03b
k19631966 <56188847+k19631966@users.noreply.github.com>
2019-11-19 21:16:55 +0800

    split routerConfigs, add lazy loding feature

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,62 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
+import VueRouter, {
+  RouteConfig
+} from 'vue-router';
 import Home from '../views/Home.vue';
-import Routerrc from './router.json';
-
+import routerrcs from './router.json';
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
+const routes: RouteConfig[] = [{
+  path: '/',
+  name: 'home',
+  component: Home
+}];
+
+// route level code-splitting
+// this generates a separate chunk (about.[hash].js) for this route
+// which is lazy-loaded when the route is visited.
+const routerJson: Routerrc[] = JSON.parse(JSON.stringify(routerrcs));
+routes.concat(createRouteConfigs(routerJson));
+
+interface Routerrc {
+  path: string;
+  name: string;
+  webpackChunkName: string;
+  sourcePath: string;
+}
+
+// Getting all configurations from router.json.
+function createRouteConfigs (routerrcs: Routerrc[]): RouteConfig[] {
+  let configArr: RouteConfig[] = [];
+
+  if (routerrcs === null || routerrcs === undefined) {
+    return configArr;
   }
-];
+
+  for (let index in routerrcs) {
+    if (index !== null) {
+      let routerrc = routerrcs[index];
+
+      let routeConfig: RouteConfig = {
+        path: routerrc.path,
+        name: routerrc.name,
+        component: < () => Promise < typeof import('*.vue') >> getComponentByPath(routerrc.sourcePath, routerrc.webpackChunkName)
+      };
+
+      configArr.push(routeConfig);
+    }
+  }
+  return configArr;
+}
+
+// Dynamically import components using component names in router.json.
+function getComponentByPath (sourcePath: string, webpackChunkName ? : string): ((value: any) => any) | null | undefined {
+  if (sourcePath !== undefined && webpackChunkName !== undefined) {
+    return () => import( /* webpackChunkName: "`${webpackChunkName}`" */ `${sourcePath}`);
+  } else if (sourcePath !== undefined) {
+    return () => import(`${sourcePath}`);
+  }
+}
 
 const router = new VueRouter({
   mode: 'history',
